b1dcfd157fdbf5c6dab56541881e602cb0b17be9
santoshr1982 <santoshr1982@yahoo.com>
2016-04-16 09:10:19 +0530

    Added files via upload

---
+++ MainHelloWorld.java
@@ -0,0 +1,14 @@
+package helloworld.rsantosh.com.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainHelloWorld extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_hello_world);
+
+    }
+}
